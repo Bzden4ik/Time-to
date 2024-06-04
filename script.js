6b2d608fc4cb4e1e8680f00349f29962e35aeed7
Bzden <158495797+Bzden4ik@users.noreply.github.com>
2024-06-04 20:27:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function updateCountdown() {
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));  // Исправленная строка
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     if (distance < 0) {
